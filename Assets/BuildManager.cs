fb93a38da3ed3ee14fa60a5f129f5be1245c8050
Daynero <iamdaynero@gmail.com>
2021-07-21 16:10:03 +0300

    add build manager to create a new turret on the map

---
+++ Assets/BuildManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.Log("More than one BuildManager in the scene!");
+            return;
+        }
+
+        instance = this;
+    }
+
+    public GameObject standartTurretPrefab;
+
+    void Start()
+    {
+        turretToBuild = standartTurretPrefab;
+    }
+
+    private GameObject turretToBuild;
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
