8ef816184b513396e387a7c45c7ceeec3e647798
Daynero <iamdaynero@gmail.com>
2021-07-23 15:23:09 +0300

    added missle and particles effects for it

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class Bullet : MonoBehaviour
 {
     private Transform target;
 
     public float speed = 70f;
+    public float explosionRadius = 0f;
     public GameObject impactEffect;
 
     public void Seek(Transform _target)
@@ -31,14 +33,48 @@ public class Bullet : MonoBehaviour
         }
 
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+        transform.LookAt(target);
 
     }
 
     void HitTarget()
     {
         var effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
-        Destroy(effectIns, 2f);
-        Destroy(target.gameObject);
+        Destroy(effectIns, 5f);
+
+        if (explosionRadius > 0f)
+        {
+            Explode();
+        }
+        else
+        {
+            Damage(target);
+        }
+       
         Destroy(gameObject);
+    }
+
+    void Explode()
+    {
+        var colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+
+        foreach (var collider in colliders)
+        {
+            if (collider.tag == "Enemy")
+            {
+                Damage(collider.transform);
+            }
+        }
+    }
+
+    void Damage(Transform enemy)
+    {
+        Destroy(enemy.gameObject);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, explosionRadius);
     }
 }
