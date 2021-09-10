9d9b52c8d77c96c24d3f08d3c507a8aa9b781a03
Roman Kravchenko <r.kravchenko@themindstudios.com>
2021-09-10 16:34:21 +0300

    Change method press button in choose lvl menu

---
+++ Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelector : MonoBehaviour
+{
+    [SerializeField] private SceneFader fader;
+    [SerializeField] private Button[] levelButtons;
+
+    private string levelName;
+    
+    private void Start()
+    {
+        for (int i = 0; i < levelButtons.Length - 1; i++)
+        {
+            levelName = (i < 10) ? "Level0" + (i + 1) : "Level" + (i + 1);
+            
+            levelButtons[i].onClick.AddListener(delegate { SelectLevel(levelName); });
+        }
+        
+        int levelReached = PlayerPrefs.GetInt("levelReached", 1);
+
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            if (i + 1 > levelReached)
+                levelButtons[i].interactable = false;
+        }
+    }
+
+    public void SelectLevel (string levelName)
+    {
+        fader.FadeTo(levelName);
+    }
+}
