c0681b3be2795dd00ccde665a11cd904df2c4a4c
Roman Kravchenko <r.kravchenko@themindstudios.com>
2021-09-14 16:03:10 +0300

    Realization dragging and zooming camera with limiting bounds

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,79 +1,170 @@
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UIElements;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private float panSpeed = 3f;
-    [SerializeField] private float panBorderThickness = 10f;
-    [SerializeField] private float scrollSpeed = 5f;
-    [SerializeField] private float minY = 10f;
-    [SerializeField] private float maxY = 80f;
+    [SerializeField] private float maxPanSpeed = 0.1f;
+    [SerializeField] private float minPanSpeed = 0.035f;
+    [SerializeField] private float minFieldOfView = 25f;
+    [SerializeField] private float maxFieldOfView = 65f;
+    [SerializeField] private float maxZBoundsLimit = 20f;
+    [SerializeField] private float maxXBoundsLimit  = 30f;
     [SerializeField] private Camera camera;
 
-    private bool drag = false;
+    private float currentZBoundsLimit;
+    private float currentXBoundsLimit;
+    private Vector3 posStartCam;
+    private bool isDragging = false;
+    private bool isZooming = false;
+    private float currentPanSpeed = 0.1f;
     private Vector3 delta;
     private Vector3 startPos;
-    
-    private void Update()
+    private Vector3 initialTouchPosition;
+    private Vector3 initialCameraPosition;
+    private Vector3 initialTouch0Position;
+    private Vector3 initialTouch1Position;
+    private Vector3 initialMidPointScreen;
+    private float initialFieldOfView;
+
+    private void Start()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector3 startCamPos = new Vector3(camera.transform.position.z, camera.transform.position.x, 0);
-            startPos = Input.mousePosition;
-            Debug.Log("startCamPos = " + startCamPos + "   startPos = " + startPos);
-            delta = startPos + startCamPos;
-
-        } 
-        else
-        if (Input.GetMouseButton(0))
-        {
-            Debug.Log("delta = " + delta + "   startPos = " + startPos);
-            Vector3 pos = (delta - Input.mousePosition);
-            camera.transform.position = new Vector3(pos.y,camera.transform.position.y, pos.x);
-        }
-        
-        
-       
-        
-        // if (GameManager.gameIsOver)
-        // {
-        //     enabled = false;
-        //     return;
-        // }
-        
-        
-
-        // if (Input.GetKey("w") )
-        // {
-        //     transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
-        // }
-        //
-        // if (Input.GetKey("s") )
-        // {
-        //     transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
-        // }
-        //
-        // if (Input.GetKey("d") )
-        // {
-        //     transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
-        // }
-        //
-        // if (Input.GetKey("a") )
-        // {
-        //     transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
-        // }
-        //
-        // float scroll = Input.GetAxis("Mouse ScrollWheel");
-        //
-        // Vector3 pos = transform.position;
-        //
-        // pos.y -= scroll * 1000 * scrollSpeed * Time.deltaTime;
-        // pos.y = Mathf.Clamp(pos.y, minY, maxY);
-        //
-        // transform.position = pos;
+        posStartCam = transform.position;
     }
 
+    private void Update()
+    {
+        if (GameManager.gameIsOver)
+        {
+            enabled = false;
+            return;
+        }
+
+        float fieldCamera = camera.fieldOfView;
+        
+        // Change PanSpeed proportionally FieldOfView
+        currentPanSpeed = (maxPanSpeed - minPanSpeed) * (fieldCamera - minFieldOfView) 
+            / (maxFieldOfView - minFieldOfView) + minPanSpeed;
+        
+        // Change limiting boundaries proportionally FieldOfView
+        currentXBoundsLimit =  Mathf.Abs(fieldCamera - maxFieldOfView) * maxXBoundsLimit  / (maxFieldOfView - minFieldOfView) ;
+        currentZBoundsLimit =  Mathf.Abs(fieldCamera - maxFieldOfView) * maxZBoundsLimit  / (maxFieldOfView - minFieldOfView) ;
+        
+        if (Input.touchCount == 1)
+        {
+            DragCamera();
+        }
+
+        if (Input.touchCount == 2)
+        {
+            ZoomAndDragCamera();
+        }
+        else
+        {
+            isZooming = false;
+        }
+    }
+
+    private void DragCamera()
+    {
+        Touch touch0 = Input.GetTouch(0);
+
+        if (IsTouching(touch0))
+        {
+            if (!isDragging)
+            {
+                initialTouchPosition = touch0.position;
+                initialCameraPosition = transform.position;
+
+                isDragging = true;
+            }
+            else
+            {
+                delta = (Vector3) touch0.position - initialTouchPosition;
+                Vector3 newPos = initialCameraPosition;
+
+                newPos.z += delta.x * currentPanSpeed;
+                newPos.x -= delta.y * currentPanSpeed;
+                
+                transform.position = ClampNewCameraPosition(newPos);
+            }
+        }
+            
+        if (!IsTouching(touch0))
+        {
+            isDragging = false;
+        }
+    }
+
+    private void ZoomAndDragCamera()
+    {
+        isDragging = false;
+
+            Touch touch0 = Input.GetTouch(0);
+            Touch touch1 = Input.GetTouch(1);
+
+            if (!isZooming)
+            {
+                initialTouch0Position = touch0.position;
+                initialTouch1Position = touch1.position;
+                initialCameraPosition = transform.position;
+                initialFieldOfView = camera.fieldOfView;
+                initialMidPointScreen = (touch0.position + touch1.position) / 2;
+
+                isZooming = true;
+            }
+            else
+            {
+                transform.position = initialCameraPosition;
+                camera.fieldOfView = initialFieldOfView;
+
+                float scaleFactor = GetScaleFactor(touch0.position,
+                                                   touch1.position,
+                                                   initialTouch0Position,
+                                                   initialTouch1Position);
+                
+                Vector2 currentMidPoint = (touch0.position + touch1.position) / 2;
+                Vector3 initialPointWorldBeforeZoom = initialMidPointScreen;
+
+                camera.fieldOfView = Mathf.Clamp(initialFieldOfView / scaleFactor, minFieldOfView, maxFieldOfView);
+
+                Vector3 initialPointWorldAfterZoom = initialMidPointScreen;
+                Vector2 initialPointDelta = initialPointWorldBeforeZoom - initialPointWorldAfterZoom;
+
+                Vector2 oldAndNewPointDelta = (Vector3)currentMidPoint - initialMidPointScreen;
+
+                Vector3 newPos = initialCameraPosition;
+                
+                newPos.z += (oldAndNewPointDelta.x - initialPointDelta.x) * currentPanSpeed;
+                newPos.x -= (oldAndNewPointDelta.y - initialPointDelta.y) * currentPanSpeed;
+
+                transform.position = ClampNewCameraPosition(newPos);
+            }
+    }
+
+    private Vector3 ClampNewCameraPosition(Vector3 newPos)
+    {
+        newPos.z = Mathf.Clamp(newPos.z,  posStartCam.z - currentZBoundsLimit, posStartCam.z + currentZBoundsLimit);
+        newPos.x = Mathf.Clamp(newPos.x,  posStartCam.x - currentXBoundsLimit, posStartCam.x + currentXBoundsLimit);
+
+        return newPos;
+    }
+
+    private bool IsTouching(Touch touch)
+    {
+        return touch.phase == TouchPhase.Began ||
+               touch.phase == TouchPhase.Moved ||
+               touch.phase == TouchPhase.Stationary;
+    }
     
-    
+    private float GetScaleFactor(Vector2 position1, Vector2 position2, Vector2 oldPosition1, Vector2 oldPosition2)
+    {
+        float distance = Vector2.Distance(position1, position2);
+        float oldDistance = Vector2.Distance(oldPosition1, oldPosition2);
+
+        if (oldDistance == 0 || distance == 0)
+        {
+            return 1.0f;
+        }
+
+        return distance / oldDistance;
+    }
 }
